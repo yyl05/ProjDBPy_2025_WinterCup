76e69663b57ed5e9448afe00cb4fe1162a563d4d
yuri <yuri.lima@eduvaud.ch>
2025-12-25 03:54:36 +0100

    débougage de certaines fonction et ajout desfonctions

--- ProjDBPy_2025_WinterCup_python_code/def_area.py
+++ ProjDBPy_2025_WinterCup_python_code/def_area.py
@@ -1,5 +1,7 @@
 import mysql.connector
 import os
+from datetime import *
+
 
 my_password= os.environ.get('my_password')
 
@@ -8,15 +10,6 @@ def wintercup_open():
                                    database='tournament_wintercup',autocommit=True, buffered=True)
 
 db_connection= wintercup_open()
-
-def menu() :
-    print("Que voulez-vous faire comme opération? \n")
-    print("1. SELECT")
-    print("2. INSERT")
-    print("3. UPDATE")
-    print("4. DELETE")
-    print("5. INSERT WITH CSV FILE")
-    print("6. QUIT")
 
 def select_players():
     query = "SELECT * FROM players"
@@ -33,6 +26,7 @@ def select_teams():
     rows = cursor.fetchall()
     cursor.close()
     return rows
+
 
 def select_highschool():
     query = "SELECT * FROM highschool"
@@ -66,6 +60,31 @@ def select_coaches():
     rows = cursor.fetchall()
     cursor.close()
     return rows
+
+def select_data_teams():
+    get_teams = select_teams()
+    for team in get_teams:
+        print(f"{team[0]} - {team[1]} - {team[2]}")
+
+def select_data_highschool():
+    get_highschool = select_highschool()
+    for highschool in get_highschool:
+        print(f"{highschool[0]} - {highschool[1]} - {highschool[2]} - {highschool[3]}")
+
+def select_data_coachs():
+    get_coach = select_coaches()
+    for coach in get_coach:
+        print(f"{coach[0]} - {coach[1]} - {coach[2]} - {coach[3]} - {coach[4]}")
+
+def select_data_players():
+    get_player = select_players()
+    for player in get_player:
+        print(f"\n{player[0]} - {player[1]} - {player[2]} - {player[3]} - {player[4]} - {player[5]} - {player[6]} - {player[7]}")
+
+def select_data_tournament():
+    get_tournament = select_tournament()
+    for tournament in get_tournament:
+        print(f"{tournament[0]} - {tournament[1]} - {tournament[2]} - {tournament[3]} - {tournament[4]} - {tournament[5]}")
 
 def add_data_highschool(name, adress, prefecture):
     query = "INSERT INTO highschool (name, adress, prefecture) values (%s, %s, %s)"
@@ -116,9 +135,9 @@ def add_data_statistics(statistics_points, statistics_assists, statistics_reboun
     return insert_id
 
 def get_team_id_from_name(name):
-    query = "SELECT name FROM teams WHERE id = %s"
+    query = "SELECT id FROM teams WHERE name = %s"
     cursor = db_connection.cursor()
-    cursor.execute(query,(name))
+    cursor.execute(query,(name,))
     if cursor.rowcount == 0:
         cursor.close()
         return None
@@ -128,9 +147,9 @@ def get_team_id_from_name(name):
         return row[0]
     
 def get_highschool_id_from_name(name):
-    query = "SELECT name FROM highschool WHERE id = %s"
+    query = "SELECT id FROM highschool WHERE name = %s"
     cursor = db_connection.cursor()
-    cursor.execute(query,(name))
+    cursor.execute(query,(name,))
     if cursor.rowcount == 0:
         cursor.close()
         return None
@@ -139,10 +158,10 @@ def get_highschool_id_from_name(name):
         cursor.close()
         return row[0]
     
-def get_player_id_from_name(name):
-    query = "SELECT name FROM players WHERE id = %s"
+def get_player_id_from_name(lastname,firstname):
+    query = "SELECT id FROM players WHERE lastname = %s and firstname = %s"
     cursor = db_connection.cursor()
-    cursor.execute(query,(name))
+    cursor.execute(query,(lastname,firstname))
     if cursor.rowcount == 0:
         cursor.close()
         return None
@@ -151,10 +170,10 @@ def get_player_id_from_name(name):
         cursor.close()
         return row[0]
     
-def get_coach_id_from_name(name):
-    query = "SELECT name FROM coaches WHERE id = %s"
+def get_coach_id_from_name(lastname,firstname):
+    query = "SELECT id FROM coaches WHERE name = %s and firstname = %s"
     cursor = db_connection.cursor()
-    cursor.execute(query,(name))
+    cursor.execute(query,(lastname,firstname))
     if cursor.rowcount == 0:
         cursor.close()
         return None
@@ -202,7 +221,7 @@ def update_coaches_from_id(id, new_value):
     cursor.close()
 
 def update_tournament_from_id(id, column, new_value):
-    columns_tournament = ["name","place","ranking","matchs_day"]
+    columns_tournament = ["ranking","matchs_day"]
 
     if column not in columns_tournament:
         raise ValueError("Aucune colonne identifié")
@@ -254,3 +273,304 @@ def delete_coaches_from_id(coaches_id):
     cursor.execute(query, (coaches_id,))
     cursor.close()
     print(f"Le coach {coaches_id} supprimée avec succès.")
+
+# Pas d'idée pour l'instant de comment code cette partie qui a une table intermediaire
+def select_player_statistics():
+    pass
+
+
+def insert_datas_highschool():
+    while True:
+        print("Rentrez les informations de l'école: ")
+        name_highschool = input("Nom de l'école: ")
+        addres_highschool = input("Adresse de l'école: ")
+        prefecture_highschool = input("Nom prefecture highschool: ")
+        try:
+            add_data_highschool(name_highschool, addres_highschool, prefecture_highschool)
+            print("\nLes données ont été bien enregistrer")
+            break
+        except ValueError as err:
+            print(err)
+
+def insert_datas_teams():
+    while True:
+        print("Rentrez les informations de la team: ")
+        name_team = input("Nom de la team: ")
+        while True:
+            creation_date = input("Entrez la date sous format [yyyy-mm-dd]")
+            try:
+                creation_date=datetime.strptime(creation_date,'%Y-%m-%d')
+                break
+            except ValueError:
+                print("La date doit être sous format [yyyy-mm-dd]")
+        try:
+            add_data_teams(name_team,creation_date)
+            print("\nLes données ont été bien enregistrer")
+            break
+        except ValueError as err:
+            print(err)
+
+def insert_datas_players():
+    while True:
+        print("Rentrez les informations du joueur: ")
+        lastname_player = input("Nom: ")
+        firstname_player = input("Prénom: ")
+        while True:
+            try:
+                age = int(input("Âge: "))
+                break
+            except ValueError:
+                print("L'âge doit être un chiffre")
+        while True:
+            try:
+                tall = int(input("Taille en cm: "))
+            except ValueError:
+                print("La taille doit être un chiffre")
+            if len(tall) != 3:
+                print("Veuillez rentrer votre taille en cm s'ils vous plaît")
+            else:
+                break
+
+        while True:
+            position = input("Poste du joueur: ")
+            if len(position) != 2:
+                print("Veuillez entrer seulement les lettres de votre poste")
+            else:
+                break
+
+        while True:
+            try:
+                highschool_id = int(input("Highschool ID: "))
+                break
+            except ValueError:
+                print("Le highschool id doit être un chiffre")
+        
+        while True:
+            try:
+                teams_id = int(input("Team id: "))
+                break
+            except ValueError:
+                print("La team id doit être un chiffre")
+        
+        try:
+            add_data_players(lastname_player, firstname_player, age, tall, position, highschool_id, teams_id)
+        except ValueError as err:
+            print(err)
+
+def insert_datas_coachs():
+    while True:
+        print("Veuillez rentrer les informations du coach")
+        lastname_coach = input("Nom: ")
+        firstname_coach = input("Prénom: ")
+        while True:    
+            type_coach = int(input("Veuillez rentre 0 pour coach principale ou 1 pour coach adjoint: "))
+            if type_coach != 0 or type_coach != 1:
+                print("Veuillez saisir 0 ou 1 ")
+            else:
+                break
+        
+        while True:
+            try:    
+                team_id = int(input("Rentrer l'id de la team qu'il appartient: "))
+                break
+            except ValueError:
+                print("Veuillez rentrer un chiffre") 
+        try:
+            add_data_coachs(lastname_coach,firstname_coach, type_coach, team_id)
+            break
+        except ValueError as err:
+            print(err)
+
+
+
+def insert_datas_team_with_csv():
+    with open("/home/yuri/ProjDBPy_2025_WinterCup/csv/teams.csv", encoding='latin-1') as file:
+        next(file)
+        for line in file:   
+            row=line.strip().split(",")
+            if get_team_id_from_name(row[0]):
+                print(f"La team: {row[0]} existe déjà")
+            else:
+                add_data_teams(row[0], row[1])
+
+        print("Les données ont bien été enregistrées")
+
+def insert_datas_highschool_with_csv():
+    with open("../csv/highschool.csv") as file:
+        next(file)
+        for line in file:
+            row=line.strip().split(",")
+            if get_highschool_id_from_name(row[0]):
+                print(f"La highschool: {row[0]} existe déjà")
+            else:
+                add_data_highschool(row[0], row[1], row[2])
+
+def insert_datas_player_with_csv():
+    with open("../csv/players.csv") as file:
+        next(file)
+        for line in file:
+            row=line.strip().split(",")
+            highschool_id = get_highschool_id_from_name(row[5])
+            team_id = get_team_id_from_name(row[6])
+            if highschool_id and team_id:
+                add_data_players(row[0], row[1], row[2], row[3], row[4], highschool_id, team_id)
+            elif not highschool_id:
+                print(f"La highschool: {row[5]} n'existe pas pour la ligne: {row}")
+            else:
+                print(f"La team: {row[6]} n'existe pas pour la ligne: {row}")
+
+def insert_datas_coaches_with_csv():
+    with open("../csv/coaches.csv") as file:
+        next(file)
+        for line in file:
+            row=line.strip().split(",")
+            team_id = get_team_id_from_name(row[3])
+            if team_id:
+                add_data_coachs(row[0], row[1], row[2], team_id)
+            else:
+                print(f"la team: {row[3]} n'existe pas pour la ligne: {row}")
+
+def insert_datas_tournament_with_csv():
+    with open("../csv/tournament.csv") as file:
+        next(file)
+        for line in file:
+            row=line.strip().split(",")
+            team_id = get_team_id_from_name(row[3])
+            if team_id:
+                add_data_tournament(row[0],row[1],row[2],row[3],team_id)
+            else:
+                print(f"La team: {row[4]}")
+
+
+
+def update_data_team():
+    columns_team = ["name"]
+    print("Voici les colonnes modifiables: ",', '.join(columns_team))
+    
+    select_data_teams()
+
+    new_name_team = input("Choisissez le nouveau nom de l'équipe: ")
+    while True:
+        try:
+            id_team = int(input("Selectionnez l'ID: "))
+            break
+        except ValueError as err:
+            print(err)
+    update_teams_from_id(id_team, new_name_team)
+    print(f"\nNouveau nom: {new_name_team} à été modifié pour l'ID: {id_team}")
+
+def update_data_players():
+    columns_player = ["age","tall","position"]
+    print("\nVoici les colonnes modifiables: ",', '.join(columns_player))
+
+    select_data_players()
+
+    while True:
+        choice_column = input("\nChoisissez la colonne qui sera modifiée: ")
+        if choice_column in columns_player:
+            break
+        else:
+            print("\nVeuillez choisir une colonne modifiables: ",', '.join(columns_player))
+    if choice_column.lower() == columns_player[0]:
+        while True:    
+            try:
+                new_age = int(input("Nouvelle âge: "))
+                id_player = int(input("ID: "))
+                break
+            except ValueError as err:
+                print(err)
+        update_players_from_id(id_player,choice_column,new_age)
+        print(f"Nouvelle age: {new_age} à été modifiée pour l'ID: {id_player}")
+    elif choice_column.lower() == columns_player[1]:
+
+        while True:
+            try:
+                new_height = int(input("Nouvelle taille: "))
+                id_player = int(input("ID: "))
+                break
+            except ValueError as err:
+                print(err)
+
+        update_players_from_id(id_player,choice_column,new_height)
+        print(f"Nouvelle taille: {new_height} à été modifiée pour l'ID: {id_player}")
+        
+    elif choice_column.lower() == columns_player[2]:
+    
+        while True:
+            new_position = int(input("Nouvelle poste: "))
+            if len(new_position) != 2:
+                print("Veuillez rentrer uniquement les 2 lettres du poste")
+            else:
+                break
+
+        while True:
+            try:
+                id_player = int(input("ID: "))
+                break
+            except ValueError as err:
+                print(err)
+        update_players_from_id(id_player,choice_column,new_height)
+        print(f"Nouvelle taille: {new_height} à été modifiée pour l'ID: {id_player}")
+
+def update_data_coach():
+    columns_coach = ["type"]
+    print("Voici les colonnes modifiables: ",', '.join(columns_coach))
+    
+    select_data_coachs()
+    while True:
+    
+        new_type_coach = int(input("Selectionnez 0 pour coach principale ou 1 pour coach adjoint: "))
+
+        if new_type_coach != 0 or new_type_coach != 1:
+            print("Choisissez 0 ou 1 ")
+        else:
+            break
+
+            
+    while True:
+        try:
+            coach_id = int(input("Selectionnez l'ID: "))
+            break
+        except ValueError as err:
+            print(err)
+    update_coaches_from_id(coach_id, new_type_coach)
+    print(f"\nNouveau nom: {new_type_coach} à été modifié pour l'ID: {coach_id}")    
+    pass
+
+def update_data_tournament():
+    columns_tournament = ["ranking","matchs_day"]
+    print("Voici les colonnes modifiables: ",', '.join(columns_tournament))
+    
+    select_data_tournament()
+
+    while True:
+        choice_column = input("\nChoisissez la colonne qui sera modifiée: ")
+        if choice_column in columns_tournament:
+            break
+        else:
+            print("\nVeuillez choisir une colonne modifiables: ",', '.join(columns_tournament))
+    
+    if choice_column.lower() == columns_tournament[0]:
+        while True:
+            try:
+                new_ranking = int(input("New rank: "))
+                id_tournament = int(input("Id: "))
+                break
+            except ValueError:
+                print("Veuillez saisir une donnée de type int (chiffre).")
+        update_tournament_from_id(id_tournament,choice_column,new_ranking)
+        print(f"Nouveau rank: {new_ranking} à été modifiée pour l'ID: {id_tournament}")
+    elif choice_column.lower() == columns_tournament[1]:
+        while True:
+            try:
+                new_match_day = int(input("New match day: "))
+                id_tournament = int(input("Id: "))
+                break
+            except ValueError:
+                print("\nVeuillez saisir une donnée de type int (chiffre)")
+        update_tournament_from_id(id_tournament,choice_column,new_match_day)
+        print(f"Nouveau jour: {new_match_day} à été modifiée pour l'Id: {id_tournament}")
+
+
+
+
--- ProjDBPy_2025_WinterCup_python_code/main.py
+++ ProjDBPy_2025_WinterCup_python_code/main.py
@@ -1,7 +1,152 @@
 from def_area import *
 
-def main():
-    pass
+def main() :
+    while True:
+        print("\n1. SELECT")
+        print("2. INSERT")
+        print("3. UPDATE")
+        print("4. DELETE")
+        print("5. INSERT WITH CSV FILE")
+        print("6. QUIT")
+        while True:
+            try:    
+                choice_usr = int(input("\nQue voulez-vous faire? ")) 
+                break
+            except ValueError as err:
+                print(err)
+            
+        if choice_usr == 1:
+            print("\n1. Teams")
+            print("2. HighSchool")
+            print("3. Players")
+            print("4. Coachs")
+            print("5. Tournament")
+            while True:
+                try:
+                    table_choice = int(input("\nQuelle table voulez-vous afficher? "))
+                    break
+                except ValueError:
+                    print("\nVeuillez choisir un numéro s'ils vous plaît.")
+            
+            if table_choice == 1:
+                print("")
+                select_data_teams()
+                print("\nVoici les datas pour la table teams.")
+                break
+            elif table_choice == 2:
+                print("")
+                select_data_highschool()
+                print("\nVoici les datas pour la table highschool.")
+                break
+            elif table_choice == 3:
+                print("")
+                select_data_players()
+                print("\nVoici les datas pour la table players.")
+                break
+            elif table_choice == 4:
+                print("")
+                select_data_coachs()
+                print("\nVoici les datas pour la table coachs.")
+                break
+            elif table_choice == 5:
+                print("")
+                select_data_tournament()
+                print("\nVoici les datas pour la table tournament.")
+                break
+            else:
+                print("Choisissez un chiffre qui appartient au menu s'ils vous plaît.")
+        elif choice_usr == 2:
+            print("\n1. Teams")
+            print("2. HighSchool")
+            print("3. Players")
+            print("4. Coachs")
+
+            while True:
+                try:
+                    table_choice = int(input("\nDans quelle table voulez-vous insérer des données? "))
+                    break
+                except ValueError:
+                    print("\nVeuillez choisir un numéro s'ils vous plaît.")
+            if table_choice == 1:
+                insert_datas_teams()
+                break
+            elif table_choice == 2:
+                insert_datas_highschool()
+                break
+            elif table_choice == 3:
+                insert_datas_players()
+                break
+            elif table_choice == 4:
+                insert_datas_coachs()
+                break
+            else:
+                print("Choisissez un chiffre qui appartient au menu s'ils vous plaît.")
+        elif choice_usr == 3:
+            print("\n1. Teams")
+            print("2. Players")
+            print("3. Coachs")
+            print("4. Tournament")
+
+            while True:
+                try:
+                    table_choice = int(input("\nDans quelle table voulez-vous modifier? "))
+                    break
+                except ValueError:
+                    print("\nVeuillez choisir un numéro s'ils vous plaît.")
+            if table_choice == 1:
+                update_data_team()
+                break
+            elif table_choice == 2:
+                update_data_players()
+                break
+            elif table_choice == 3:
+                update_data_coach()
+                break
+            elif table_choice == 4:
+                update_data_tournament()
+                break
+            else:
+                print("Choisissez un chiffre qui appartient au menu s'ils vous plaît.")
+
+
+        elif choice_usr == 5:
+            print("\n1. Teams")
+            print("2. HighSchool")
+            print("3. Players")
+            print("4. Coachs")
+            print("5. Tournament")
+
+
+            while True:
+                try:
+                    table_choice = int(input("\nDans quelle table voulez-vous insérer en utilisant un fichier csv? "))
+                    break
+                except ValueError:
+                    print("\nVeuillez choisir un numéro s'ils vous plaît.")
+            if table_choice == 1:
+                insert_datas_team_with_csv()
+                break
+            elif table_choice == 2:
+                insert_datas_highschool_with_csv()
+                break
+            elif table_choice == 3:
+                insert_datas_player_with_csv()
+                break
+            elif table_choice == 4:
+                insert_datas_coaches_with_csv()
+                break
+            elif table_choice == 5:
+                insert_datas_tournament_with_csv()
+            else:
+                print("Choisissez un chiffre qui appartient au menu s'ils vous plaît.")   
+                        
+            
+
+#Continuer le code du menu pour l'usr et rajouter les def delete, updates, et certains selects
+
+#Si possible coder une petite interface dans le terminal(Apprendre comment faire ca pendant les vancances.)
+
+#name de tournament est unique pour l'instant faudra l'enlever aprés
 
 if __name__ == '__main__':
     main()
